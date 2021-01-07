b204d670dcd9ab7749879a76d17c44ffa4b48ede
RegisGRB <42929082+RegisGRB@users.noreply.github.com>
2021-01-07 14:13:32 +0100

    Update ButtonPwaInstall

--- src/components/ui/PWAInstall/PwaInstall.js
+++ src/components/ui/PWAInstall/PwaInstall.js
@@ -3,18 +3,41 @@ import React, { useEffect, useState } from "react";
 import { LangContext } from "../../../context";
 const PwaInstall = ({ className }) => {
   const LangContextx = React.useContext(LangContext);
-  const [supportsPWA, setSupportsPWA] = useState(!(window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone || document.referrer.includes("android-app://")));
-  const [promptInstall, setPromptInstall] = useState(null);
+  const [supportsPWA, setSupportsPWA] = useState();
+  let deferredPrompt;
 
+  useEffect(() => {
+    setSupportsPWA(
+      !(
+        window.matchMedia("(display-mode: standalone)").matches ||
+        window.navigator.standalone ||
+        document.referrer.includes("android-app://")
+      )
+    );
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      // showInstallPromotion();
+    });
+  });
   const onClick = (evt) => {
     evt.preventDefault();
     console.log("we are being triggered :D");
-    if (!promptInstall) {
-      return;
-    }
-    promptInstall.prompt();
-    setSupportsPWA(false);
-    setPromptInstall(evt);
+    // hideMyInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+      setSupportsPWA(false);
+    });
   };
 
   return (
